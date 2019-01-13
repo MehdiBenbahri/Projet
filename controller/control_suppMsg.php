fc27e68bffab60e2ca831088cec2ff064613f448
MehdiBenbahri <taoufikvar@gmailcom>
2019-01-13 20:21:22 +0100

    -Correction d'une faille de sécurité dans les controller de suppression de message / d'utilisateur.

--- controller/control_suppMsg.php
+++ controller/control_suppMsg.php
@@ -1,12 +1,12 @@
 <?php
-if (isset($_SESSION['co'])) {
+if (isset($_SESSION['co']) && $bdd->isAdmin($_SESSION['co'])) {
     $idPost = filter_var($_POST["idPost"], FILTER_SANITIZE_STRING);
     $idTopic = filter_var($_POST["idTopic"], FILTER_SANITIZE_STRING);
     $bdd->removePost($idPost);
 
     header('Location: topic?=' . $idTopic);
 } else {
-    $_SESSION['rapport']->createRapport("Vous devez être connecteé !", "#f4ca41", "Erreur : ", "#f46842");
+    $_SESSION['rapport']->createRapport("Vous devez être connecté en tant que Administrateur !", "#f4ca41", "Erreur : ", "#f46842");
     header('Location: accueil');
     exit();
 }
--- controller/control_suppUser.php
+++ controller/control_suppUser.php
@@ -1,12 +1,12 @@
 <?php
-if (isset($_SESSION['co'])) {
+if (isset($_SESSION['co']) && $bdd->isAdmin($_SESSION['co'])) {
     $idUser = filter_var($_POST["idUser"], FILTER_SANITIZE_STRING);
     $idTopic = filter_var($_POST["idTopic"], FILTER_SANITIZE_STRING);
     $bdd->removeUser($idUser);
 
     header('Location: topic?=' . $idTopic);
 } else {
-    $_SESSION['rapport']->createRapport("Vous devez être connecteé !", "#f4ca41", "Erreur : ", "#f46842");
+    $_SESSION['rapport']->createRapport("Vous devez être connecté en tant que Administrateur !", "#f4ca41", "Erreur : ", "#f46842");
     header('Location: accueil');
     exit();
 }
